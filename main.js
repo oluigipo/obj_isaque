830f162e20249a1fc17a8e4c7eebb02ce701ae25
crickkin <37256927+crickkin@users.noreply.github.com>
2019-10-09 15:12:53 -0300

    Abstração de Comando - mute

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const discordServer = require('./src/constants');
+const Moderation = require('./src/Components/Moderation');
 
 const isEvalEnabled = false;
 const specialInvite = 'p9WN6Rx';
 let prefix = "!!";
 const roleToAdd = "585871344718184458";
 const roleMuted = "568171976556937226";
-// const cursoLink = "https://www.udemy.com/course/torne-se-um-desenvolvedor-de-jogos-2d/?couponCode=GM_STUDIO2";
 const shitpostChannel = "playground";
 const admins = ["373670846792990720", "330403904992837632", "412797158622756864", "457020706857943051", "290130764853411840", "141958545397645312"];
 const maxMuteTime = 60 * 60 * 24 * 7 * 2;
@@ -24,68 +24,7 @@ const wait = require('util').promisify(setTimeout);
 
 // Argumentos padrões: (Message, Arguments)
 const Commands = {
-	mute: function (msg, args) {
-		if (!isAdmin(msg.author)) return;
-		if (args.length < 2) {
-			msg.channel.send(`Uso correto: \`\`\`\n${prefix}mute @user\n${prefix}mute 30m @user\n${prefix}mute @user1 @user2...\n${prefix}mute 1h @user1 @user2...\n\`\`\``);
-			return;
-		}
-
-		let duration = -1;
-		let _t;
-		let _d;
-		if (args.length > 2 && args[1][0] !== '<') {
-			_t = parseInt(args[1]);
-			_d = args[1][String(_t).length];
-
-			duration = 1;
-			switch (_d) {
-				case 'w': duration *= 7;
-				case 'd': duration *= 24;
-				case 'h': duration *= 60;
-				case 'm': duration *= 60; break;
-				case 's': break;
-				default:
-					msg.channel.send(`${_t + _d} isn't a valid time`);
-					return;
-			}
-
-			duration *= _t;
-
-			if (duration > maxMuteTime) {
-				msg.channel.send(`Tempo máximo para mute: 2w (2 semanas)`);
-				return;
-			}
-		}
-
-		msg.mentions.members.forEach(m => {
-			if (m.manageable) {
-				m.addRole(roleMuted);
-				if (duration > 0) {
-					setTimeout(() => m.removeRole(roleMuted), duration * 1000);
-					msg.channel.send(`O usuário ${m.user.tag} foi mutado por ${_formateTime(_t, _d)} com sucesso.`).catch(console.error);
-				} else {
-					msg.channel.send(`O usuário ${m.user.tag} foi mutado com sucesso.`).catch(console.error);
-				}
-			} else {
-				msg.channel.send(`Não é possível mutar o usuário ${m.user.tag}.`).catch(console.error);
-			}
-		});
-
-		function _formateTime(_time, _str) {
-			let s = `${_time} `;
-			switch (_str) {
-				case 'w': s += "semana"; break;
-				case 'd': s += "dia"; break;
-				case 'h': s += "hora"; break;
-				case 'm': s += "minuto"; break;
-				case 's': s += "segundo"; break;
-			}
-			if (_time > 1) s += 's';
-
-			return s;
-		}
-	},
+	mute: Moderation.mute,
 	unmute: function (msg, args) {
 		if (!isAdmin(msg.author)) return;
 		if (args.length < 2) {
---
+++ src/Components/Moderation.js
@@ -0,0 +1,68 @@
+const roleMuted = "568171976556937226";
+
+function mute (msg, args) {
+    if (!isAdmin(msg.author)) return;
+    if (args.length < 2) {
+        msg.channel.send(`Uso correto: \`\`\`\n${prefix}mute @user\n${prefix}mute 30m @user\n${prefix}mute @user1 @user2...\n${prefix}mute 1h @user1 @user2...\n\`\`\``);
+        return;
+    }
+
+    let duration = -1;
+    let _t;
+    let _d;
+    if (args.length > 2 && args[1][0] !== '<') {
+        _t = parseInt(args[1]);
+        _d = args[1][String(_t).length];
+
+        duration = 1;
+        switch (_d) {
+            case 'w': duration *= 7;
+            case 'd': duration *= 24;
+            case 'h': duration *= 60;
+            case 'm': duration *= 60; break;
+            case 's': break;
+            default:
+                msg.channel.send(`${_t + _d} isn't a valid time`);
+                return;
+        }
+
+        duration *= _t;
+
+        if (duration > maxMuteTime) {
+            msg.channel.send(`Tempo máximo para mute: 2w (2 semanas)`);
+            return;
+        }
+    }
+
+    msg.mentions.members.forEach(m => {
+        if (m.manageable) {
+            m.addRole(roleMuted);
+            if (duration > 0) {
+                setTimeout(() => m.removeRole(roleMuted), duration * 1000);
+                msg.channel.send(`O usuário ${m.user.tag} foi mutado por ${_formateTime(_t, _d)} com sucesso.`).catch(console.error);
+            } else {
+                msg.channel.send(`O usuário ${m.user.tag} foi mutado com sucesso.`).catch(console.error);
+            }
+        } else {
+            msg.channel.send(`Não é possível mutar o usuário ${m.user.tag}.`).catch(console.error);
+        }
+    });
+
+    function _formateTime(_time, _str) {
+        let s = `${_time} `;
+        switch (_str) {
+            case 'w': s += "semana"; break;
+            case 'd': s += "dia"; break;
+            case 'h': s += "hora"; break;
+            case 'm': s += "minuto"; break;
+            case 's': s += "segundo"; break;
+        }
+        if (_time > 1) s += 's';
+
+        return s;
+    }
+}
+
+module.exports = {
+    mute,
+}
