75d72c4df9b3364929ca4d49a3c0ef1858d6334a
crickkin <37256927+crickkin@users.noreply.github.com>
2019-10-09 14:37:30 -0300

    teste de abstração

---
+++ src/constants/index.js
@@ -0,0 +1,13 @@
+const id = '630891905852506123';
+const specialInvite = 'MbFMYVJ';
+const prefix = '+';
+const roleToAdd = "630917234826543105";
+const roleToAddAnyway = "630936042224222229";
+
+module.exports = {
+    id,
+    specialInvite,
+    prefix,
+    roleToAdd,
+    roleToAddAnyway
+}
