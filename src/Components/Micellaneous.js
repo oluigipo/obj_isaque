f8f5e2be107c08382dd00b0ddd74ea4b6d2c5412
tuasnega <36678917+Luig1B@users.noreply.github.com>
2019-10-10 13:22:51 -0300

    Update Micellaneous.js

--- src/Components/Micellaneous.js
+++ src/Components/Micellaneous.js
@@ -1,12 +1,29 @@
-const { shitpostChannel } = require('../constants');
+const { shitpostChannel, sintaxErrorMessage } = require('../constants');
 
-function ulon (msg) {
+function ulon(msg) {
     if (msg.channel.name !== shitpostChannel) return;
     const qnt = Math.floor(Math.random() * 200 + 2);
     msg.channel.send("UL" + "O".repeat(qnt) + "N")
         .catch(console.error);
 }
 
+function emoji(msg, args) {
+    if (args.length < 2) {
+        msg.channel.send(sintaxErrorMessage);
+        return;
+    }
+
+    const e = msg.guild.emojis.find(a => a.name === args[1]);
+    if (e === undefined) {
+        msg.channel.send(`O emoji \`${args[1]}\` é inválido.`);
+        return;
+    }
+
+    msg.channel.send(`${e}`); // ${msg.author}: 
+    msg.delete();
+}
+
 module.exports = {
     ulon,
+    emoji
 }
