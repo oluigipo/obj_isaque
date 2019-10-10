8753add37b9177f18e0fae22470bc14dbc50ab16
tuasnega <36678917+Luig1B@users.noreply.github.com>
2019-10-09 23:33:51 -0300

    Update do link do curso

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,7 +4,7 @@ const prefix = '+';
 const roleToAdd = "630917234826543105";
 const roleToAddAnyway = "630936042224222229";
 
-const cursoLink = "https://www.udemy.com/course/torne-se-um-desenvolvedor-de-jogos-2d/?couponCode=GM_STUDIO2";
+const cursoLink = "http://bit.ly/35iBP6O";
 
 module.exports = {
     id,
