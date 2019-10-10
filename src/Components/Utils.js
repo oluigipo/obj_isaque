d11c0d3599bc3c7af8e2b900bb7dc01e7b1979a6
tuasnega <36678917+Luig1B@users.noreply.github.com>
2019-10-10 13:22:28 -0300

    Update Utils.js

--- src/Components/Utils.js
+++ src/Components/Utils.js
@@ -1,17 +1,35 @@
 const discordServer = require('../constants');
-const { shitpostChannel, cursoLink } = discordServer;
+const { shitpostChannel, cursoLink, youtubeLink } = discordServer;
 
-async function ping (msg) {
+async function ping(msg) {
     if (msg.channel.name !== shitpostChannel) return;
     const m = await msg.channel.send("...");
     m.edit(`\`Bot Latency:\` ${m.createdTimestamp - msg.createdTimestamp}ms\n\`API Latency:\` ${Math.round(msg.client.ping)}ms`);
 }
 
-function curso (msg) {
+function curso(msg) {
     msg.channel.send(`${msg.author} Aqui está o link do curso: ${cursoLink}`);
+}
+
+function nonetube(msg) {
+    msg.channel.send(`${msg.author} Aqui está o link do youtube: ${youtubeLink}`);
+}
+
+function help(msg, args) {
+    let s;
+    if (args.length < 2) {
+        s = discordServer.helpMessage;
+    } else {
+        s = discordServer.helpCommands[args[1]];
+        if (s === undefined) s = "Comando desconhecido.";
+    }
+
+    msg.channel.send(`${msg.author} ${s}`);
 }
 
 module.exports = {
     ping,
     curso,
+    help,
+    nonetube
 }
