fb15d3e59d2cdfd3d9155b34ca2275a18cdcb4ae
Luig1B <LuigiBenedetti2005@gmail.com>
2019-11-26 13:30:17 -0300

    yay

--- build/src/components/Cassino/Loja.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const itens = [];
-const Loja = {
-    upgrade(msg, args) {
-        let user = args.length > 1 ? args[1] : msg.author.id;
-    }
-};
-exports.default = Loja;
--- build/src/constants/ServerInfo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.specialInvite = 'p9WN6Rx';
-exports.roleAluno = "585871344718184458";
-exports.roleDefault = "585871344718184458";
-exports.roleBot = "589633044013514784";
-exports.serverID = "507550989629521922";
-exports.shitpostChannel = "playground";
