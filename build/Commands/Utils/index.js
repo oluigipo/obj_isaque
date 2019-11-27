87e6b89db3fefd5671f6d31ca58addf1415a27e4
Luig1B <LuigiBenedetti2005@gmail.com>
2019-11-27 19:48:00 -0300

    rem

--- build/Commands/Utils/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var ping_1 = __importDefault(require("./ping"));
-var help_1 = __importDefault(require("./help"));
-var nonetube_1 = __importDefault(require("./nonetube"));
-exports.default = [ping_1.default, help_1.default, nonetube_1.default];
