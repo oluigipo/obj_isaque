0c7290025acace1910ede39e6e66e53bccdf5b06
tuasnega <36678917+Luig1B@users.noreply.github.com>
2020-03-13 23:50:30 -0300

    update

--- src/Commands/Micellaneus/danki.ts
+++ src/Commands/Micellaneus/danki.ts
@@ -1,19 +1,26 @@
 import { Command, Arguments, Server, CommonMessages, Permission } from "../../definitions";
-import { Message, TextChannel } from "discord.js";
+import { Message, TextChannel, RichEmbed } from "discord.js";
 
 const images: string[] = [
 	"https://cdn.discordapp.com/attachments/553933292542361601/633732233156362270/Danki_Boude.png",
 	"https://cdn.discordapp.com/attachments/553933292542361601/633732285547544605/gamedev.png",
 	"https://tenor.com/view/danki-danki-code-danki-cry-curso-danki-gif-15268437",
-	"https://tenor.com/view/danki-dankicode-gif-15270906"
+	"https://tenor.com/view/danki-dankicode-gif-15270906",
+	"https://cdn.discordapp.com/attachments/507551338616586264/641328025488588800/sanfjasfjasf-1.gif"
 ];
 let dankiImageCurrent = 0;
 
 export default <Command>{
 	run: (msg: Message, args: Arguments): void => {
 		dankiImageCurrent = (dankiImageCurrent + 1) % images.length;
-
-		msg.channel.send(`${msg.author} ${images[dankiImageCurrent]}`);
+		
+		let final = new RichEmbed();
+		final.image = images[dankiImageCurrent];
+		final.color = Server.botcolor;
+		final.author = { name: msg.member.displayName, icon_url: msg.author.avatarURL };
+		final.footer = { text: msg.client.user.username, icon_url: msg.client.user.avatarURL };
+		
+		msg.channel.send(final);
 	},
 	permissions: Permission.Shitpost,
 	aliases: ["danki"],
