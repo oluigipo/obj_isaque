b44e38e9e7f9c56828a164396435393234560f5b
Luig1B <LuigiBenedetti2005@gmail.com>
2020-08-12 17:26:33 -0300

    limpando

--- src/Commands/Moderation/index.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from "../../definitions";
-
-import mute from "./mute";
-import unmute from "./unmute";
-import ban from "./ban";
-import kick from "./kick";
-import eval from "./eval";
-
-export default <Command[]>[mute, unmute, ban, kick, eval];
--- src/Commands/index.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from "../definitions";
-import Moderation from "./Moderation";
-import Utils from "./Utils";
-import Micellaneus from "./Micellaneus";
-//import Cassino from "./Cassino";
-
-import Shop from "./Shop";
-
-export default <Command[]>[...Moderation, ...Utils, ...Micellaneus, Shop];
