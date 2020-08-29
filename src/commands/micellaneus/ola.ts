16e84ed1a123bae877740b7a9b85fa42ad3ebf41
Luig1B <LuigiBenedetti2005@gmail.com>
2020-08-29 08:22:33 -0300

    time

--- src/commands/micellaneus/ola.ts
+++ src/commands/micellaneus/ola.ts
@@ -1,15 +1,15 @@
-import { Command, Arguments, Permission, discordErrorHandler } from "../../defs";
+import { Command, Arguments, Permission, discordErrorHandler, Time } from "../../defs";
 import { Message } from "discord.js";
 
 export default <Command>{
 	async run(msg: Message, args: Arguments, raw: string[]) {
-		if (!msg.member || !msg.member.voice.channel)
+		if (!msg.member || !msg.member.voice.channel || msg.client.voice?.connections.size)
 			return;
 
 		msg.member.voice.channel.join().then(connection => {
 			const dispatcher = connection.play("assets/ola-pessoal.ogg");
 
-			const action = () => connection.disconnect();
+			const action = () => setTimeout(() => connection.disconnect(), Time.second);
 			dispatcher.on("finish", action);
 			dispatcher.on("error", action);
 		}).catch(discordErrorHandler);
