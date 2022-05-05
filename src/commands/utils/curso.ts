f9b7b8fd7fc82ed5ea2efd9506b7a63b80336a53
Jorge Santos <33334289+cenowador@users.noreply.github.com>
2022-05-05 10:37:20 -0300

    (update) added ceno's affiliate link

--- src/commands/utils/curso.ts
+++ src/commands/utils/curso.ts
@@ -6,7 +6,8 @@ const links: any = {
 	"330403904992837632": "https://go.hotmart.com/Y17948280P",
 	"338717274032701460": "https://go.hotmart.com/V17855634L",
 	"454330688267878401": "https://go.hotmart.com/E39119891D",
-	"194463676558737408": "https://go.hotmart.com/Q31250063W"
+	"194463676558737408": "https://go.hotmart.com/Q31250063W",
+	"366993075513589771": "https://go.hotmart.com/J70404800U"
 };
 
 export default <Command>{
